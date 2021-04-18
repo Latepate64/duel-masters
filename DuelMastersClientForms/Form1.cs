93c1d1d02cca82df31692ee3ea05c2fd6be33f1b
Latepate64 <lauri.koriseva@gmail.com>
2021-04-18 13:05:45 +0300

    Added project DuelMastersClientForms which provides GUI for sending interface requests to the server.

---
+++ DuelMastersClientForms/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuelMastersClientForms
+{
+    public partial class Form1 : Form
+    {
+        const int Port = 80;
+        const string IP = "127.0.0.1";
+
+        TcpClient _tcpClient;
+
+        delegate void SafeCallDelegate(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosed += Form1_FormClosed;
+            OutputTextBox.AppendText("Welcome!");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            TryDisconnect();
+        }
+
+        private void TryDisconnect()
+        {
+            _tcpClient.GetStream().Close();
+            _tcpClient.Close();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress address = IPAddress.Parse(IP);
+                if (_tcpClient == null)
+                {
+                    _tcpClient = new();
+                }
+                WriteNewLine("Connecting to the server...");
+                Task task = _tcpClient.ConnectAsync(address, Port);
+                task.Wait();
+                WriteNewLine("Connected to the server.");
+                Task receive = new Task(() => ReceiveMessages());
+                receive.Start();
+            }
+            catch (Exception ex)
+            {
+                WriteNewLine(ex.Message);
+            }
+        }
+
+        void ReceiveMessages()
+        {
+            while (_tcpClient.Connected)
+            {
+                byte[] buffer = new byte[1024];
+                int received;
+                try
+                {
+                    received = _tcpClient.GetStream().Read(buffer);
+                }
+                catch (System.IO.IOException ex)
+                {
+                    WriteNewLine(ex.Message);
+                    break;
+                }
+                if (received > 0)
+                {
+                    WriteNewLine(Encoding.ASCII.GetString(buffer, 0, received));
+                }
+            }
+            WriteNewLine("You have been disconnected from the server.");
+        }
+
+        private void WriteNewLine(string text)
+        {
+            if (OutputTextBox.InvokeRequired)
+            {
+                OutputTextBox.Invoke(new SafeCallDelegate(WriteNewLine), new object[] { text });
+            }
+            else
+            {
+                OutputTextBox.AppendText(Environment.NewLine + text);
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes(InputTextBox.Text);
+                ValueTask write = _tcpClient.GetStream().WriteAsync(bytes);
+                InputTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                WriteNewLine(ex.Message);
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TryDisconnect();
+                _tcpClient = null;
+                WriteNewLine("Disconnected from the server.");
+            }
+            catch (Exception ex)
+            {
+                WriteNewLine(ex.Message);
+            }
+        }   
+    }
+}
---
+++ DuelMastersServer/Server.cs
@@ -0,0 +1,92 @@
+﻿using System.Net.Sockets;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DuelMastersServer
+{
+    internal class Client
+    {
+        internal TcpClient TCPClient { get; set; }
+        internal string Name { get; set; }
+    }
+
+    class Server
+    {
+        static TcpListener _tcpListener;
+        static readonly Client _client1 = new() { Name = "Player1" };
+        static readonly Client _client2 = new() { Name = "Player2" };
+
+        static void Main(string[] args)
+        {
+            const int Port = 80;
+            const string IP = "127.0.0.1";
+            IPAddress address = IPAddress.Parse(IP);
+            _tcpListener = new(address, Port);
+            _tcpListener.Start();
+            Console.WriteLine($"Waiting for {_client1.Name} to connect...");
+            _client1.TCPClient = _tcpListener.AcceptTcpClient();
+            Console.WriteLine($"{_client1.Name} connected.");
+            Task task1 = new(() => ReceiveMessages(_client1));
+            task1.Start();
+            Console.WriteLine($"Waiting for {_client2.Name} to connect...");
+            _client2.TCPClient = _tcpListener.AcceptTcpClient();
+            Console.WriteLine($"{_client2.Name} connected.");
+            Task task2 = new(() => ReceiveMessages(_client2));
+            task2.Start();
+            List<Task> tasks = new()
+            {
+                task1,
+                task2,
+            };
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("All clients have disconnected. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void ReceiveMessages(Client client)
+        {
+            while (client.TCPClient.Connected)
+            {
+                byte[] buffer = new byte[1024];
+                int received;
+                try
+                {
+                    received = client.TCPClient.GetStream().Read(buffer);
+                }
+                catch (System.IO.IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+                if (received > 0)
+                {
+                    string msg = $"{client.Name}: {Encoding.ASCII.GetString(buffer, 0, received)}";
+                    Console.WriteLine(msg);
+                    byte[] bytes = Encoding.ASCII.GetBytes(msg);
+                    foreach (Client activeClient in GetActiveClients())
+                    {
+                        activeClient.TCPClient.GetStream().Write(bytes);
+                    }
+                }
+            }
+            Console.WriteLine($"{client.Name} disconnected.");
+        }
+
+        static IEnumerable<Client> GetActiveClients()
+        {
+            List<Client> clients = new();
+            if (_client1.TCPClient != null && _client1.TCPClient.Connected)
+            {
+                clients.Add(_client1);
+            }
+            if (_client2.TCPClient != null && _client2.TCPClient.Connected)
+            {
+                clients.Add(_client2);
+            }
+            return clients;
+        }
+    }
+}
